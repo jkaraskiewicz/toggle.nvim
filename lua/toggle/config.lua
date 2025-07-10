a617ca27fdba3d1eeb7abddbbfaef70aa48c5c00
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-07-10 17:23:11 +0200

    toggle update

--- lua/toggle/config.lua
+++ lua/toggle/config.lua
@@ -45,6 +45,51 @@ M.defaults = {
       end,
       desc = 'Color column',
     },
+    virtual_diagnostics = {
+      enabled = true,
+      key = 'v',
+      values = {
+        {
+          desc = 'lines:all',
+          value = 'all',
+        },
+        {
+          desc = 'lines:current',
+          value = 'current',
+        },
+        {
+          desc = 'text',
+          value = 'text',
+        },
+        {
+          desc = 'off',
+          value = 'off',
+        },
+      },
+      toggle = function(value)
+        local display_text = value == 'text'
+        local display_lines = value == 'all' or value == 'current'
+        local lines_config
+
+        if not display_lines then
+          lines_config = false
+        elseif value == 'all' then
+          lines_config = {
+            current_line = false,
+          }
+        else
+          lines_config = {
+            current_line = true,
+          }
+        end
+
+        vim.diagnostic.config({
+          virtual_text = display_text,
+          virtual_lines = lines_config,
+        })
+      end,
+      desc = 'Virtual diagnostics',
+    },
   },
 }
 
